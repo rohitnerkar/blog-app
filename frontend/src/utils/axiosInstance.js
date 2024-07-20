2d813b0df4c040183880ca42a7c47c23b94fa549
Rohit Nerkar <rohitnerkar1997@gmail.com>
2024-07-20 16:03:00 +0530

    .

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-const apiUrl = import.meta.env.VITE_API_URL;
+// const apiUrl = import.meta.env.VITE_API_URL;
 
-const axiosInstance = axios.create({ baseURL: `${apiUrl}/api/v1` });
+const axiosInstance = axios.create({ baseURL: "http://localhost:8000/api/v1" });
 
 axiosInstance.interceptors.request.use((req) => {
   const stringifyBlogData = window.localStorage.getItem("blogData");
