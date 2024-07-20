6296a94735ea4c75517788190368ebb1887b9127
Rohit Nerkar <rohitnerkar1997@gmail.com>
2024-07-20 15:53:23 +0530

    .

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -40,6 +40,7 @@ function App() {
           <Route path="/signup" element={<Signup />} />
           <Route path="/login" element={<Login />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/" element={<Navigate to="/login" />} />
         </Route>
       </Routes>
       <ToastContainer />
